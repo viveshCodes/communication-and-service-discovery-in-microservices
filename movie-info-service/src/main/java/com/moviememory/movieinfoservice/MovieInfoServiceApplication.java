0e6f91bb1063dda998d98e8da237418d1711cf53
viveshCodes <viveshofficial@gmail.com>
2021-09-28 21:51:50 +0530

    Fix eureka client dependencies in last commit

--- movie-info-service/src/main/java/com/moviememory/movieinfoservice/MovieInfoServiceApplication.java
+++ movie-info-service/src/main/java/com/moviememory/movieinfoservice/MovieInfoServiceApplication.java
@@ -2,10 +2,12 @@ package com.moviememory.movieinfoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MovieInfoServiceApplication {
 
 	public static void main(String[] args) {
